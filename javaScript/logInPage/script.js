5a09880308948e36a569e87fbd964b9e0295b0c7
Osame Issa <osame.issa7@gmail.com>
2022-11-23 21:05:11 +0100

    commit for the day

--- javaScript/logInPage/script.js
+++ javaScript/logInPage/script.js
@@ -16,53 +16,82 @@ document.addEventListener("DOMContentLoaded", () => {
         logInForm.classList.remove("formHidden");
         createAccountForm.classList.add("formHidden");
     });
-});
+})
 
 // signingIn
 const logInForm = document.querySelector("#logIn"); 
     
 logInForm.addEventListener("submit", e =>   {
     e.preventDefault()
+
     // get username/password values
     let username = document.getElementById("username").value
     let password = document.getElementById("password").value
+
     // store values in localStorage
-    localStorage.setItem("#username", username);
-    localStorage.setItem("#password", password);
-    console.log("Credentials stored in localStorage");
+         localStorage.setItem("username", username);
+         localStorage.setItem("password", password);
+         console.log("Credentials stored in localStorage");
 
-    // fetch values from localStorage
-    
-
+     
 })
 
 const username = document.getElementById('username');
 const password = document.getElementById('password');
 const welcome = document.getElementById("welcome");
 const formTask = document.getElementById("formTask");
-const task = document.getElementById("task");
+const signOutBtn = document.getElementById("signOutBtn");
 
 
-const users = [ 
-    {"username": "janne", "password": "test"},
-    {"username": "joe", "password": "biden"},
-    {"username": "donald", "password": "trump"},
- ];
+    const users = [ 
+        {"username": "Janne", "password": "test"},
+        {"username": "Joe", "password": "Doe"},
+        {"username": "Sergey", "password": "Nazarov"},
+        {"username": "Bob", "password": "Lazar"},
+        {"username": "Mario", "password": "64"},    
+    ];
 
 loggingIn = () => { 
-const loggingIn = document.getElementById('formBtn');
-    console.log('Checking credentials..');
+    const loggingIn = document.getElementById('formBtn');
+        console.log('Checking credentials..');
 
     for (i = 0; i < users.length; i++) {
       if (username.value == users[i].username && password.value == users[i].password) {
+        console.log(username.value + " logged in.")
         logInForm.classList.add("formHidden");
         formTask.classList.remove("formHidden");
-        task.classList.remove("formHidden");
+        signOutBtn.classList.remove("formHidden");
         formTask.innerHTML = "Welcome, " + username.value;
         return true;
-        
       }
     }
+    // if not -> display error message
     invalid.classList.remove("formHidden");
+    console.log("Invalid credentials");
+}
 
+// fetch values from localStorage, "autologin"
+checkCredentials = () => {
+    for (i = 0; i < users.length; i++) {
+    let storedUsername = localStorage.getItem("username");
+    let storedPassword = localStorage.getItem("password");
+        const checkCredentials = document.querySelector("checking");
+        if (storedUsername == users[i].username && storedPassword == users[i].password) {
+        console.log("Welcome back, " + storedUsername);
+        logInForm.classList.add("formHidden");
+        formTask.classList.remove("formHidden");
+        signOutBtn.classList.remove("formHidden");
+        formTask.innerHTML = "Welcome, " + storedUsername;
+        return true;
+        }
+
+    //clear cache when signing out
+     document.getElementById("signOutBtn").addEventListener("click" , () => {
+        localStorage.removeItem("username", username);
+        localStorage.removeItem("password", password);
+        console.log("Cleared localStorage");
+    })
+
+    //push newly created accounts into array
+  }
 }
