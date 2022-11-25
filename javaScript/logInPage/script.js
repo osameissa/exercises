ffbe70cf1a4bb0fb416660fdef36fd717c049983
Osame Issa <osame.issa7@gmail.com>
2022-11-25 04:38:31 +0100

    js touch-up

--- javaScript/logInPage/script.js
+++ javaScript/logInPage/script.js
@@ -1,7 +1,20 @@
-//linkLogIn -> signUpForm
-//linkSignUp -> logInForm
+// Selectors
+const username = document.getElementById('username');
+const password = document.getElementById('password');
+const welcome = document.getElementById("welcome");
+const signOutBtn = document.getElementById("signOutBtn");
+const logInForm = document.querySelector("#logIn");
 
+// [username,password]
+const users = [ 
+{"username": "Janne", "password": "test"},
+{"username": "Joe", "password": "Doe"},
+{"username": "Sergey", "password": "Nazarov"},
+{"username": "Bob", "password": "Lazar"},
+{"username": "Mario", "password": "64"}];
 
+// Event-listeners
+// Switching between logInForm & createAccountForm
 document.addEventListener("DOMContentLoaded", () => {
     const logInForm = document.querySelector("#logIn");
     const createAccountForm = document.querySelector("#createAccount");
@@ -9,91 +22,72 @@ document.addEventListener("DOMContentLoaded", () => {
     document.querySelector("#linkCreateAccount").addEventListener("click", e => {
         e.preventDefault();
         logInForm.classList.add("formHidden");
-        createAccountForm.classList.remove("formHidden");   
-    });
+        createAccountForm.classList.remove("formHidden");});
 
-    document.querySelector("#linkLogIn").addEventListener("click", e => {
-        e.preventDefault();
-        logInForm.classList.remove("formHidden");
-        createAccountForm.classList.add("formHidden");
-    });
-     //clear cache when clicking on signOutBtn
-     document.getElementById("signOutBtn").addEventListener("click" , () => {
-        localStorage.clear();
-        console.log("Cleared localStorage");
-    })
-})
-
-// get username, password & store in localStorage
-const logInForm = document.querySelector("#logIn"); 
+        document.querySelector("#linkLogIn").addEventListener("click", e => {
+            e.preventDefault();
+            logInForm.classList.remove("formHidden");
+            createAccountForm.classList.add("formHidden"
+        )
+    }
+)
     
+// Clear localStorage when clicking on signOutBtn
+            document.getElementById("signOutBtn").addEventListener("click" , () => {
+                localStorage.clear();
+                console.log("Cleared localStorage");
+            }
+        )
+    }
+)
+
+// Store username, password in localStorage
 logInForm.addEventListener("submit", e =>   {
     e.preventDefault()
-
-    // get username/password values
     let username = document.getElementById("username").value
     let password = document.getElementById("password").value
+    localStorage.setItem("username", username);
+    localStorage.setItem("password", password);
+    console.log("Credentials stored in localStorage");
+    }
+)
 
-    // store values in localStorage
-         localStorage.setItem("username", username);
-         localStorage.setItem("password", password);
-         console.log("Credentials stored in localStorage");
-
-     
-})
-
-const username = document.getElementById('username');
-const password = document.getElementById('password');
-const welcome = document.getElementById("welcome");
-const signOutBtn = document.getElementById("signOutBtn");
-
-    // array
-    const users = [ 
-        {"username": "Janne", "password": "test"},
-        {"username": "Joe", "password": "Doe"},
-        {"username": "Sergey", "password": "Nazarov"},
-        {"username": "Bob", "password": "Lazar"},
-        {"username": "Mario", "password": "64"},    
-    ];
-
+// Functions
 loggingIn = () => { 
     const loggingIn = document.getElementById('formBtn');
-        console.log('Checking credentials..');
+    console.log('Checking credentials..');
 
     for (i = 0; i < users.length; i++) {
-      if (username.value == users[i].username && password.value == users[i].password) {
-        console.log(username.value + " logged in.")
-        logInForm.classList.add("formHidden");
-        formTask.classList.remove("formHidden");
-        signOutBtn.classList.remove("formHidden");
-        signOutBtn.innerHTML = '<i class="fa-solid fa-arrow-right-from-bracket"></i>';
-        formTask.innerHTML = "Welcome, " + username.value;
-        return true;
-      }
+        if (username.value == users[i].username && password.value == users[i].password) {
+            console.log(username.value + " logged in.")
+            logInForm.classList.add("formHidden");
+            formTask.classList.remove("formHidden");
+            signOutBtn.classList.remove("formHidden");
+            signOutBtn.innerHTML = '<i class="fa-solid fa-arrow-right-from-bracket"></i>';
+            formTask.innerHTML = "Welcome, " + username.value;
+            return true;
+        }
     }
-    // if not -> display error message
+
+// If invalid credentials -> display error message
     invalid.classList.remove("formHidden");
     console.log("Invalid credentials");
 }
 
-// fetch values from localStorage, "autologin"
+// Autologin: fetch values from localStorage & cross-check
 checkCredentials = () => {
     for (i = 0; i < users.length; i++) {
-    let storedUsername = localStorage.getItem("username");
-    let storedPassword = localStorage.getItem("password");
+        let storedUsername = localStorage.getItem("username");
+        let storedPassword = localStorage.getItem("password");
         const checkCredentials = document.querySelector("checking");
         if (storedUsername == users[i].username && storedPassword == users[i].password) {
-        console.log("Welcome back, " + storedUsername);
-        logInForm.classList.add("formHidden");
-        formTask.classList.remove("formHidden");
-        signOutBtn.classList.remove("formHidden");
-        signOutBtn.innerHTML = '<i class="fa-solid fa-arrow-right-from-bracket"></i>';
-        formTask.innerHTML = "Welcome, " + storedUsername;
-        return true;
+            console.log("Welcome back, " + storedUsername);
+            logInForm.classList.add("formHidden");
+            formTask.classList.remove("formHidden");
+            signOutBtn.classList.remove("formHidden");
+            signOutBtn.innerHTML = '<i class="fa-solid fa-arrow-right-from-bracket"></i>';
+            formTask.innerHTML = "Welcome, " + storedUsername;
+            return true;
         }
-
-    
-    //push newly created accounts into array
-
-  }
+    }
 }
